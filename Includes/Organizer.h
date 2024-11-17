7731315680024ff38b2d4ff29e5fca76a38522a5
Youssef Mohammad <153318445+DEVOLOPER-1@users.noreply.github.com>
2024-11-17 23:53:26 +0200

    irgazizernclass emthods

---
+++ Includes/Organizer.h
@@ -0,0 +1,36 @@
+//
+// Created by youss on 17-Nov-24.
+//
+
+#ifndef ORGANIZER_H
+#define ORGANIZER_H
+#include <iostream>
+#include <fstream>
+
+class Organizer {
+
+private:
+    void loadInputFile(string fileName)
+
+    void simulateTimeStep(int timeStep)
+
+    void handleCarArrival(Car* car)
+
+    void assignPatientToCar(Patient* patient, Car* car)
+
+    void collectStatistics()
+
+    void produceOutputFile(string fileName)
+
+    void callUIUpdate(int timeStep)
+
+
+
+
+public:
+    
+};
+
+
+
+#endif //ORGANIZER_H
