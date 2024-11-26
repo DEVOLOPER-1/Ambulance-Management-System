b56e8e88943f6e0950bbb7e334a9413e0744228a
ahmed2005Ihab <ahmedihabbaher6@gmail.com>
2024-11-27 00:16:10 +0200

    functions in UI class

--- Includes/UI.h
+++ Includes/UI.h
@@ -14,31 +14,48 @@ using namespace std;
 
 class UI {
 public:
-    void DisplayTimeStep(int TimeStep) {
-        cout<<"Time Step: "<< TimeStep<<endl;
-        cout<<setw(120)<<setfill('-')<<""<<endl;
+    
+    void display_timestep(int timestep) {
+        cout << "timestep: "<< timestep << endl;
+        cout << setw(120) << setfill('-') << "" << endl;
     }
 
-   void DisplayHospital(Hospital* h){
-        cout << "hospital ID : " << h->GetHospitalID()<< endl; 
+   void display_hospital(Hospital* hosp){
+        cout << "hospital ID : " << hosp->GetHospitalID()<< endl; 
         cout << " status : " << endl;
-        cout << "  EP requests : " << h->GetEPRequestsCount() << endl;
-        cout << "  SP requests : " << h->GetSPRequestsCount() << endl;
-        cout << "  NP requests : " << h->GetNPRequestsCount() << endl;
+        cout << "  EP requests : " << hosp->GetEPRequestsCount() << endl;
+        cout << "  SP requests : " << hosp->GetSPRequestsCount() << endl;
+        cout << "  NP requests : " << hosp->GetNPRequestsCount() << endl;
     }
 
-    void DisplaySystemStatus(LinkedQueue<Car*> availableCars, priQueue<Car*> busyCars, LinkedQueue<Request*> pendingRequests){
-        
-        cout << "available cars : " << endl;
-        availableCars.print(); // print linked queue of available cars
-        cout << endl;
-        cout << "busy cars : " << endl;
-        busyCars.print(); // print priority queue of busy cars
-        cout << endl;
-        cout << "pending requests:" << endl;
-        pendingRequests.print(); // print linked queue of pending requests
-        cout << endl;
+      void display_out_cars(LinkedQueue<Car*> out_cars){ // show out cars
+        cout << "out cars : ";
+        if (out_cars.isEmpty()){
+            cout << "no out cars";
+        } 
+        else{
+            out_cars.print(); 
+        }
     }
+
+    void display_back_cars(LinkedQueue<Car*> back_cars){ // show back cars
+        cout << "back cars : ";
+        if (back_cars.isEmpty()){
+            cout << "no back cars";
+        } 
+        else{
+            back_cars.print(); 
+        }
+    }
+
+    void print_output_screen(int timestep, Hospital* hosp, LinkedQueue<Car*> out_cars, LinkedQueue<Car*> back_cars){
+        display_timestep(timestep); // Show timestep
+        display_hospital(hosp); // Show hospital status
+        display_out_cars(out_cars);   // Show out cars
+        display_back_cars(back_cars); // Show back cars
+    }
+
+ 
 };
 
 
